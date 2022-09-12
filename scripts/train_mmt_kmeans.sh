23fa2afe0f6c6af934a32ab4dacd748775cb677c
YanzuoLu <horn18520856657@foxmail.com>
2022-09-11 23:46:10 -0400

    Rearrange folder and add environment file

--- scripts/train_mmt_kmeans.sh
+++ scripts/train_mmt_kmeans.sh
@@ -11,7 +11,7 @@ if [ $# -ne 4 ]
 fi
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python examples/mmt_train_kmeans.py -dt ${TARGET} -a ${ARCH} --num-clusters ${CLUSTER} \
+python mmt_train_kmeans.py -dt ${TARGET} -a ${ARCH} --num-clusters ${CLUSTER} \
 	--num-instances 4 --lr 0.00035 --iters 400 -b 64 --epochs 40 \
 	--soft-ce-weight 0.5 --soft-tri-weight 0.8 --dropout 0 \
 	--init-1 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-1/model_best.pth.tar \
